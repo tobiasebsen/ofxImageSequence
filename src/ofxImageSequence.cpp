70ad8a25750035174f96099519dfb9a8d675f168
Tobias Ebsen <tobiasebsen@gmail.com>
2015-06-30 18:10:05 +0200

    Added async load

--- src/ofxImageSequence.cpp
+++ src/ofxImageSequence.cpp
@@ -8,70 +8,132 @@
 
 #include "ofxImageSequence.h"
 
-void ofxImageSequence::load(string path, float framerate) {
+ofxImage::ofxImage() {
+	bIsLoaded = false;
+}
 
-    ofDirectory dir(path);
+ofPixels & ofxImage::getPixelsRef() {
+	return pixels;
+}
+
+ofTexture & ofxImage::getTextureReference() {
+	return tex;
+}
+
+void ofxImage::loadPixels(string path) {
+	ofLoadImage(pixels, path);
+	bIsLoaded = pixels.getWidth() > 0 && pixels.getHeight() > 0;
+}
+
+bool ofxImage::isPixelsLoaded() {
+	return bIsLoaded;
+}
+
+ofxImageSequence::~ofxImageSequence() {
+	waitForThread();
+}
+
+void ofxImageSequence::load(string path, float framerate, bool useRam) {
+
+    this->framerate = framerate;
+	this->path = path;
+	this->useRam = useRam;
+
+	ofDirectory dir(path);
     int n = dir.listDir();
 
     images.resize(n);
     
     for (int i=0; i<n; i++) {
         ofFile file = dir[i];
-        ofLoadImage(images[i], file.getAbsolutePath());
+		ofxImage & image = images[i];
+		ofPixels & pixels = image.getPixelsRef();
+		ofTexture & tex = image.getTextureReference();
+        image.loadPixels(file.getAbsolutePath());
+		if (!useRam) {
+			tex.allocate(pixels);
+			tex.loadData(pixels);
+			pixels.clear();
+		}
     }
     
-    this->width = images[0].getWidth();
-    this->height = images[0].getHeight();
+	this->width = images[0].getTextureReference().getWidth();
+    this->height = images[0].getTextureReference().getHeight();
+}
+
+void ofxImageSequence::loadAsync(string path, float framerate, bool useRam) {
+
     this->framerate = framerate;
+	this->path = path;
+	this->useRam = useRam;
+
+	this->startThread();
 }
 
 void ofxImageSequence::draw(int frame) {
+	draw(frame, 0., 0., 0.);
+}
+
+void ofxImageSequence::draw(int frame, float x, float y, float z) {
     if (frame >= 0 && frame < images.size()) {
-        images[frame].draw(0, 0);
+		ofxImage & image = images[frame];
+		ofPixels & pixels = image.getPixelsRef();
+		ofTexture & tex = image.getTextureReference();
+		if (!tex.isAllocated() && image.isPixelsLoaded()) {
+			tex.allocate(pixels);
+			tex.loadData(pixels);
+			if (!useRam)
+				pixels.clear();
+		}
+		if (tex.isAllocated())
+			tex.draw(x, y, z);
+		if (useRam)
+			tex.clear();
     }
 }
 
 void ofxImageSequence::drawCentered(int frame) {
+	drawCentered(frame, 0., 0., 0.);
+}
+
+void ofxImageSequence::drawCentered(int frame, float x, float y, float z) {
     if (frame >= 0 && frame < images.size()) {
-        ofTexture & tex = images[frame];
-        tex.draw(-tex.getWidth()*0.5f, -tex.getHeight()*0.5f);
+		ofxImage & image = images[frame];
+		ofPixels & pixels = image.getPixelsRef();
+		ofTexture & tex = image.getTextureReference();
+		if (pixels.isAllocated())
+			draw(frame, x-pixels.getWidth()*0.5f, y-pixels.getHeight()*0.5f, z);
+		else if (tex.isAllocated())
+			draw(frame, x-tex.getWidth()*0.5f, y-tex.getHeight()*0.5f, z);
     }
 }
 
-void ofxImageSequenceRAM::load(string path, float framerate) {
+void ofxImageSequence::threadedFunction() {
 
-    ofDirectory dir(path);
-    int n = dir.listDir();
-    
-    images.resize(n);
-    
-    for (int i=0; i<n; i++) {
-        ofFile file = dir[i];
-        ofLoadImage(images[i], file.getAbsolutePath());
-    }
-    
-    this->width = images[0].getWidth();
-    this->height = images[0].getHeight();
-    this->framerate = framerate;
-}
+	ofDirectory dir(path);
+	int n = dir.listDir();
+	images.resize(n);
 
-void ofxImageSequenceRAM::draw(int frame) {
-    if (frame >= 0 && frame < images.size()) {
-        texture.loadData(images[frame]);
-        texture.draw(0, 0);
-    }
-}
-
-void ofxImageSequenceRAM::drawCentered(int frame) {
-    if (frame >= 0 && frame < images.size()) {
-        texture.loadData(images[frame]);
-        texture.draw(-texture.getWidth()*0.5f, -texture.getHeight()*0.5f);
-    }
+	for (int i=0; i<n && isThreadRunning(); i++) {
+		ofxImage & image = images[i];
+		ofPixels & pixels = image.getPixelsRef();
+		image.loadPixels(dir.getFile(i).getAbsolutePath());
+		if (i == 0) {
+			width = pixels.getWidth();
+			height = pixels.getHeight();
+		}
+	}
+	CompleteEventArgs args;
+	args.path = path;
+	args.sequence = this;
+	if (isThreadRunning())
+		ofNotifyEvent(onLoadComplete, args);
 }
 
 void ofxImageSequencePlayer::setup(ofPtr<ofxImageSequence> & sequence) {
     this->sequence = sequence;
     this->loop = false;
+	startTime = ofGetElapsedTimef();
 }
 
 
@@ -81,19 +143,30 @@ void ofxImageSequencePlayer::play() {
 }
 
 void ofxImageSequencePlayer::setPosition(float percent) {
-    float dur = sequence->getDuration();
-    startTime = (ofGetElapsedTimef()) - percent * dur;
-    currentFrame = (int)floorf(percent * sequence->getFramerate() + 0.5f);
+
+	if (sequence == NULL)
+		return;
+
+	float dur = sequence->getDuration();
+    startTime -= percent * dur;
+    currentFrame = (int)floorf(percent * sequence->getNumFrames() + 0.5f);
+}
+
+ofxImageSequence* ofxImageSequencePlayer::getSequence() {
+	return sequence.get();
 }
 
 void ofxImageSequencePlayer::update() {
     
-    float progress = ofGetElapsedTimef() - startTime;
+	if (sequence == NULL)
+		return;
+
+    float seconds = ofGetElapsedTimef() - startTime;
     int n = sequence->getNumFrames();
-    currentFrame = (int)floorf(progress * sequence->getFramerate() + 0.5f);
+    currentFrame = (int)floorf(seconds * sequence->getFramerate() + 0.5f);
     
     if (currentFrame >= n) {
-        if (loop)
+        if (loop && n > 0)
             currentFrame %= n;
         else
             currentFrame = n - 1;
@@ -101,16 +174,19 @@ void ofxImageSequencePlayer::update() {
 }
 
 void ofxImageSequencePlayer::draw() {
-    sequence->draw(currentFrame);
+	if (sequence != NULL)
+	    sequence->draw(currentFrame);
 }
 
 void ofxImageSequencePlayer::draw(float x, float y, float z) {
     ofPushMatrix();
     ofTranslate(x, y, z);
-    sequence->draw(currentFrame);
+	if (sequence != NULL)
+	    sequence->draw(currentFrame);
     ofPopMatrix();
 }
 
 void ofxImageSequencePlayer::drawCentered() {
-    sequence->drawCentered(currentFrame);
+	if (sequence != NULL)
+	    sequence->drawCentered(currentFrame);
 }
--- src/ofxImageSequence.h
+++ src/ofxImageSequence.h
@@ -10,41 +10,60 @@
 
 #include "ofMain.h"
 
-
-class ofxImageSequence {
+class ofxImage {
 public:
+	ofxImage();
+
+	ofPixels & getPixelsRef();
+	ofTexture & getTextureReference();
+
+	void loadPixels(string path);
+	bool isPixelsLoaded();
+
+private:
+	ofPixels pixels;
+	ofTexture tex;
+	bool bIsLoaded;
+};
+
+class ofxImageSequence : public ofThread {
+public:
+	~ofxImageSequence();
     
-    virtual void load(string path, float framerate = 30.f);
-    virtual void draw(int frame);
+    virtual void load(string path, float framerate = 30.f, bool useRam = false);
+	virtual void loadAsync(string path, float framerate = 30.f, bool useRam = false);
+
+	virtual void draw(int frame);
+	virtual void draw(int frame, float x, float y, float z = 0.f);
     virtual void drawCentered(int frame);
+    virtual void drawCentered(int frame, float x, float y, float z = 0.f);
     
     int getWidth() { return width; }
     int getHeight() { return height; }
+	string getPath() { return path; }
     
     int getNumFrames() { return images.size(); }
     float getFramerate() { return framerate; }
     float getDuration() { return (float)images.size() / framerate; }
 
+	void threadedFunction();
+
+	class CompleteEventArgs : public ofEventArgs {
+	public:
+		string path;
+		ofxImageSequence* sequence;
+	};
+	ofEvent<CompleteEventArgs> onLoadComplete;
+
 protected:
     int width;
     int height;
     float framerate;
+	string path;
+	bool useRam;
 
 private:
-    vector<ofTexture> images;
-};
-
-
-class ofxImageSequenceRAM : public ofxImageSequence {
-public:
-
-    void load(string path, float framerate = 30.f);
-    void draw(int frame);
-    void drawCentered(int frame);
-
-private:
-    vector<ofPixels> images;
-    ofTexture texture;
+    vector<ofxImage> images;
 };
 
 class ofxImageSequencePlayer {
@@ -53,6 +72,8 @@ public:
     void setup(ofPtr<ofxImageSequence> & sequence);
     void play();
     void setPosition(float percent);// { this->offset = percent; }
+
+	ofxImageSequence* getSequence();
     
     void update();
     void draw();
